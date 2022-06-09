c8b40f7d0e61572ba090d48fdec6e54df6bd7fa3
Kenneth Zhao <yiguan2011@gmail.com>
2022-06-08 18:06:08 -0700

    Basic testing, hopefully this does not end in the master branch

--- src/lib/setup.js
+++ src/lib/setup.js
@@ -187,6 +187,7 @@ export default function setup(ctx, setupData) {
     const allCharacters = CHARACTERS.filter(c => c.expansion === undefined || expansions.includes(c.expansion));
     const numCharacterChoices = 3 * (numPlayers + 1) <= allCharacters.length ? 3 : 2;
     const monarchChoices = random.Shuffle(allCharacters.filter(c => c.isMonarch));
+    monarchChoices[0] = CHARACTERS.filter(c => c.name === 'Jiang Wei')[0];
     const normalCharacters = random.Shuffle(allCharacters.filter(c => !monarchChoices.includes(c)));
     const characterChoices = Object.fromEntries(playOrder.map((player, i) =>
         [player, normalCharacters.slice(numCharacterChoices * i, numCharacterChoices * (i + 1))]));
